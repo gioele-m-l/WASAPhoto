546247088100b2bc7951d62206de1b1fe805f9f0
gioele-m-l <lupi.1960450@studenti.uniroma1.it>
2023-12-13 09:51:44 +0100

    Remove get-hello-world file and modify api-handler

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -8,7 +8,7 @@ import (
 func (rt *_router) Handler() http.Handler {
 	// Register routes
 	// rt.router.GET("/", rt.getHelloWorld)
-	// rt.router.GET("/context", rt.wrap(rt.getContextReply))
+	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
 	rt.router.POST("/login", rt.wrap(rt.doLogin))
 	rt.router.PUT("/users/:username/username", rt.wrap(rt.setMyUserName))
